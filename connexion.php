6ff1a4641cdfff2fcfa76aa844754ba9f93a09e8
siliataider <siliataider@gmail.com>
2022-02-11 12:20:00 +0100

    modifs BDD

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
    try{
-      $pdo=new PDO("mysql:host=localhost;dbname=acudb","tidal@tidal.tidal","tidal!");
+      $pdo=new PDO("pgsql:host=localhost;dbname=acudb","pgtidal","tidal");
    }
    catch(PDOException $e){
       echo $e->getMessage();
