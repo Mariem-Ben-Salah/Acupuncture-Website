1fa76c4b979d4606b0678b31cb9bc0776d7ef80b
siliataider <siliataider@gmail.com>
2022-02-22 14:53:59 +0100

    changes

--- connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-   try{
-      $pdo=new PDO("pgsql:host=localhost;dbname=acudb","pgtidal","tidal");
-   }
-   catch(PDOException $e){
-      echo $e->getMessage();
-   }
-?>
--- index.php
+++ index.php
@@ -7,29 +7,22 @@
 
             <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <title>Inscription</title>
+            <title>Connexion</title>
         </head>
         <body>
+        
         <div class="login-form">
-            <?php 
-                if(isset($_GET['reg_err']))
+             <?php 
+                if(isset($_GET['login_err']))
                 {
-                    $err = htmlspecialchars($_GET['reg_err']);
+                    $err = htmlspecialchars($_GET['login_err']);
 
                     switch($err)
                     {
-                        case 'success':
-                        ?>
-                            <div class="alert alert-success">
-                                <strong>Succès</strong> inscription réussie !
-                            </div>
-                        <?php
-                        break;
-
                         case 'password':
                         ?>
                             <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe différent
+                                <strong>Erreur</strong> mot de passe incorrect
                             </div>
                         <?php
                         break;
@@ -37,41 +30,24 @@
                         case 'email':
                         ?>
                             <div class="alert alert-danger">
-                                <strong>Erreur</strong> email non valide
+                                <strong>Erreur</strong> email incorrect
                             </div>
                         <?php
                         break;
 
-                        case 'email_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email trop long
-                            </div>
-                        <?php 
-                        break;
-
-                        case 'pseudo_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> pseudo trop long
-                            </div>
-                        <?php 
                         case 'already':
                         ?>
                             <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte deja existant
+                                <strong>Erreur</strong> compte non existant
                             </div>
-                        <?php 
-
+                        <?php
+                        break;
                     }
                 }
-                ?>
+                ?> 
             
-            <form action="inscription_traitement.php" method="post">
-                <h2 class="text-center">Inscription</h2>       
-                <div class="form-group">
-                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
-                </div>
+            <form action="connexion.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
                 <div class="form-group">
                     <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
                 </div>
@@ -79,12 +55,10 @@
                     <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
                 </div>
                 <div class="form-group">
-                    <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block">Inscription</button>
+                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
                 </div>   
             </form>
+            <p class="text-center"><a href="inscription.php">Inscription</a></p>
         </div>
         <style>
             .login-form {
